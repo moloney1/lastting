2b095f02d826c77233e3fe9d800a654d10cbb329
Eric Moloney <moloneer@tcd.ie>
2019-05-29 19:13:22 +0100

    refactors

--- get.py
+++ get.py
@@ -1,7 +1,8 @@
+import os
+import json
+import pprint
 import requests
-#from creds/creds import apikey
 from creds.creds import apikey
-print(apikey)
 # http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=rj&api_key=YOUR_API_KEY&format=json
 
 BASE_URL = "http://ws.audioscrobbler.com/2.0/"
@@ -11,5 +12,13 @@ user = "recoilmoney"
 format_ = "json"
 
 req = f"{BASE_URL}?method={method}&user={user}&api_key={apikey}&format={format_}"
-r = requests.get(req)
-print(r.status_code)
+if not os.path.exists("data.json"):
+	r = requests.get(req)
+	if r.status_code == 200:
+		data = r.json()
+		with open('data.json', 'w') as f:
+			json.dump(data, f)
+else:
+	data = json.load(open('data.json','r'))
+	pp = pprint.PrettyPrinter(indent=4)
+	pp.pprint(data)
