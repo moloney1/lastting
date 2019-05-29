d7fbb14439aa1c1ef86fff760cac2b58921dced4
Eric Moloney <moloneer@tcd.ie>
2019-05-29 21:21:54 +0100

    top tracks parsing

--- api_interface.py
+++ api_interface.py
@@ -4,12 +4,13 @@ import pprint
 import requests
 from creds.creds import apikey
 
-BASE_URL = "http://ws.audioscrobbler.com/2.0/"
-
 method = "user.getrecenttracks"
 user = "recoilmoney"
 format_ = "json"
 limit = "10"
+BASE_URL = "http://ws.audioscrobbler.com/2.0/"
+
+pp = pprint.PrettyPrinter(indent=4)
 
 # return list of (artist, title, url)
 def get_recent(limit):
@@ -23,8 +24,6 @@ def get_recent(limit):
 		"api_key": apikey,
 		"format": format_
 	}
-
-	pp = pprint.PrettyPrinter(indent=4)
 	
 	if not os.path.exists('data.json'):
 		r = requests.get(BASE_URL, params=payload)
@@ -34,7 +33,7 @@ def get_recent(limit):
 	else:
 		print("loading from file..")
 		data = json.load(open("data.json", "r"))
-	#pp.pprint(data)	
+	
 	tracks = data["recenttracks"]["track"]
 	for track in tracks:
 		artist = track["artist"]["#text"]
@@ -47,5 +46,38 @@ def get_recent(limit):
 		print(f"Artist: {artist}")
 		print(f"URL: {url}")
 
+def get_top(limit):
+	period = "7day"
+	
+	payload = {
+		"method": "user.gettoptracks",
+		"user": user,
+		"limit": limit,
+		"period": period,
+		"api_key": apikey,
+		"format": format_
+	}
+	if not os.path.exists('data_top.json'):
+		r = requests.get(BASE_URL, params=payload)
+		data = r.json()
+		with open("data_top.json", "w") as f:
+			json.dump(data, f)
+	else:
+		print("loading from file..")
+		data = json.load(open("data_top.json", "r"))
+	pp.pprint(data)
+	
+	top_tracks = data["toptracks"]["track"]
+	for track in top_tracks:
+		artist = track["artist"]["name"]
+		name = track["name"]
+		url = track["url"]
+
+		print(f"Track: {name}")
+		print(f"Artist: {artist}")
+		print(f"URL: {url}")
+	
+
 if __name__ == "__main__":
-	get_recent(1)
+#	get_recent(1)
+	get_top(1)
